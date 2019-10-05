93e507085124cef0a46138c5759c728ca6d07a43
Upasana <upasanapaudyal@gmail.com>
2019-10-05 15:49:39 -0400

    fixed the operators

--- hazyCalculator.js
+++ hazyCalculator.js
@@ -1,18 +1,31 @@
 function isSkippedValue(value) {
+  if (value === undefined || value === '') {
+    return true
+  }
+  const operators = ['+', '-', '*', '/']
+  if (operators.includes(value)) {
+    return false
+  }
   return !value
 }
 
 function isNumericValue(value) {
+  if (value === null) {
+    return true
+  }
+  else if (value == '' || value === undefined) {
+    return false
+  }
   return !isNaN(value)
 }
 
 function isNothingValue(value) {
-  return value === null
+  return value === null || value === undefined
 }
 
 function isAcceptableValue(value) {
   const operators = ['+', '-', '*', '/']
-  return typeof value === Number || operators.includes(value)
+  return typeof Number(value) === 'number' || operators.includes(value)
 }
 
 function performCalculationStep(firstOperand, operator, secondOperand) {
@@ -31,8 +44,8 @@ function performCalculationStep(firstOperand, operator, secondOperand) {
 }
 
 function calculate(calculationSteps) {
-  let total
-  let operator
+  var total
+  var operator
 
   calculationSteps.forEach(nextCalculationStep => {
     if (!isAcceptableValue(nextCalculationStep)) {
